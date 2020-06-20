cf29f2785c24bf4ded1f61c35f562bad33a155bc
Tran Minh Quang <quangtm210395@gmail.com>
2020-06-20 14:37:56 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# TS-Moleculer-Boilerplate
+
+## NPM scripts
+- `npm run dev` - Start development mode (load all services locally with hot-reload & REPL)
+- `npm run build`- Uses typescript to transpile service to javascript
+- `npm start` - Start production mode (set `SERVICES` env variable to load certain services) (previous build needed)
+- `npm run lint` - Run TSLint
+- `npm run lint` - Run TSLint with --fix flag
---
+++ src/services/greeter.service.ts
@@ -0,0 +1,38 @@
+import { ServiceSchema } from 'moleculer';
+
+const GreeterService: ServiceSchema = {
+  name: 'greeter',
+  settings: {
+
+  },
+  dependencies: [],
+
+  actions: {
+    hello() {
+      return 'Hello Moleculer';
+    },
+
+    welcome: {
+      params: {
+        name: 'string',
+      },
+      handler(ctx) {
+        return `Welcome, ${ctx.params.name}`;
+      },
+    },
+  },
+
+  events: {
+
+  },
+
+  methods: {
+
+  },
+
+  created() {
+
+  },
+};
+
+export = GreeterService;
